2a70f6a0980e20dd695568fa203bd7c46e906fe1
Lauri Tuovinen <lare.tuovinen@gmail.com>
2024-01-09 10:29:37 +0200

    doned
    helepo

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+
+
 
 export default function App() {
+  const [age, setAge] = useState('');
+  const [lower, setLower] = useState(0);
+  const [upper, setUpper] = useState(0);
+
+
+function calculate(){
+    const lower = (220-age)*0.65;
+    const upper = (220-age)*0.85;
+    setLower(lower);
+    setUpper(upper);
+}
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.field}>Age</Text>
+      <TextInput style={styles.field} value = {age} onChangeText={text => setAge(text)}
+      keyboardType='decimal-pad'/>
+      <Text style={styles.field}>Limits</Text>
+      <Text style={styles.field}>{lower.toFixed()} - {upper.toFixed()}</Text>
+      <Button onPress={calculate} title="calculate"></Button>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +37,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  field:{
+    marginBottom: 10,
+  }
 });
